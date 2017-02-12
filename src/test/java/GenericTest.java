13733074eaf86946ff6e7a361202d8fde8ac012b
Marko Heini <marko.heini@gmail.com>
2017-02-12 23:37:01 +0200

    Fixed gradle-wrapper and added unit test tasks

---
+++ src/test/java/GenericTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mheini on 12/02/2017.
+ */
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+public class GenericTest {
+
+    @Test
+    public void testEqualsContract() {
+        assertNotNull("test");
+    }
+}
